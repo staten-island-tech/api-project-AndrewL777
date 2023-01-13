bbdb6a9ea63ac190cee18f62490bad7021bfb8d9
AndrewL777 <7landrew7@gmail.com>
2023-01-13 13:59:06 -0500

    found api

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-// <!-- fetch('http://example.com/movies.json')
-//   .then((response) => response.json())
-//   .then((data) => console.log(data)); -->
+const URL = "https://valorant-api.com/v1/weapons/skins";
+
+// displayIcon is img
+// displayName weapon name
