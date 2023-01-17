cfc5a6f4ad955de806300876e7ffd2903a3502f8
AndrewL777 <7landrew7@gmail.com>
2023-01-17 13:59:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 const URL = "https://valorant-api.com/v1/weapons/skins";
 
 // displayIcon is img
-// displayName weapon name
+// displayName weapon name|
+
+function Allskins() {
+  data.forEach((skin) =>
+    Dom.men.insertAdjacentHTML(
+      "beforeend",
+      `
+        <div class="card">
+        <h1 class="all">${data.displayName}</h1>
+  <img class src="${data.displayicon}" /><img>
+ 
+  </div>
+  `
+    )
+  );
+}
