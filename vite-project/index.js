1e816ab6d58a8d707656ffc3030367eb7d397046
AndrewL777 <7landrew7@gmail.com>
2023-01-18 13:57:46 -0500

    2

---
+++ vite-project/index.js
@@ -0,0 +1,38 @@
+let url = "https://valorant-api.com/v1/weapons/skins";
+
+async function getData(URL) {
+  try {
+    const reponse = await fetch(URL);
+    const data = await repsonse.json();
+    document.getElementById("api-response").textContent = data.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// let response = await fetch(url);
+// if (response.ok) {
+//   let json = await response.json();
+// } else {
+//   alert("HTTP-Error: " + response.status);
+// }
+
+console.log(data.displayName);
+
+// displayIcon is img
+// displayName weapon name|
+
+// function Allskins() {
+//   data.forEach((skin) =>
+//     Dom.men.insertAdjacentHTML(
+//       "beforeend",
+//       `
+//         <div class="card">
+//         <h1 class="all">${data.displayName}</h1>
+//   <img class src="${data.displayicon}" /><img>
+
+//   </div>
+//   `
+//     )
+//   );
+// }
