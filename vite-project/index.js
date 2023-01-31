117768788f9f66fc7f9375241212fe182a7de699
AndrewL777 <7landrew7@gmail.com>
2023-01-31 14:04:18 -0500

    123123123

--- vite-project/index.js
+++ vite-project/index.js
@@ -1,42 +1,90 @@
+const Dom = {
+  display: document.getElementById("display"),
+  Rifle: document.getElementById("Rifle"),
+  Side: document.getElementById("Side"),
+  Machine: document.getElementById("Machine"),
+  Sub: document.getElementById("Sub"),
+  all: document.getElementById("all"),
+};
+
 const APIURL = "https://valorant-api.com/v1/weapons/skins";
 
 async function getInfo() {
-    const response = await fetch(APIURL);
-    const Info = await response.json();
-    console.log(Info);
-    Info.data.forEach((skin)=> document.getElementById("display").insertAdjacentHTML(
-        "beforeend",
-        `
+  const response = await fetch(APIURL);
+  const Info = await response.json();
+  console.log(Info);
+  Info.data.forEach((skin) =>
+    document.getElementById("display").insertAdjacentHTML(
+      "beforeend",
+      `
         <div class="card">
         <h1 class="all">${skin.displayName}</h1>
   <img class src="${skin.displayIcon}" /><img>
  
   </div>
-  `));
-       
+  `
+    )
+  );
 }
 
 getInfo();
 
-// function Rifles() {
-// Info.data.filter((skin)=>skin.displayName.includes(Phantom, Vandal).document.getElementById("display").insertAdjacentHTML(
-//     "beforeend",
-//     `
-//     <div class="card">
-//     <h1 class="all">${skin.displayName}</h1>
-// <img class src="${skin.displayIcon}" /><img>
+const displaydiv = document.querySelector("#display");
 
-// </div>
-// `));
-   
-// }
+document.querySelector("#Side").addEventListener("click", function () {
+  mendiv.innerHTML = "";
+});
+
+document.querySelector("#all").addEventListener("click", function () {
+  mendiv.innerHTML = "";
+});
+
+document.querySelector("#Rifle").addEventListener("click", function () {
+  mendiv.innerHTML = "";
+});
+
+document.querySelector("#Sub").addEventListener("click", function () {
+  mendiv.innerHTML = "";
+});
+
+document.querySelector("#Sniper").addEventListener("click", function () {
+  mendiv.innerHTML = "";
+});
+document.querySelector("#Machine").addEventListener("click", function () {
+  mendiv.innerHTML = "";
+});
+
+Dom.Machine.addEventListener("click", function () {
+  machineGuns();
+});
+
+Dom.Sniper.addEventListener("click", function () {
+  sniperGuns();
+});
+
+Dom.all.addEventListener("click", function () {
+  allGuns();
+});
+
+Dom.Sub.addEventListener("click", function () {
+  subGuns();
+});
+
+Dom.Rifle.addEventListener("click", function () {
+  rifleGuns();
+});
+
+Dom.Side.addEventListener("click", function () {
+  sideGuns();
+});
 
 async function Rifles() {
-    const response = await fetch(APIURL);
-    const Info = await response.json();
-    Info.data.filter((skin)=>Info.data.displayName.includes("Phantom","Vandal")).forEach((skin)=> console.log(displayName))
-       
-    }
-
+  const response = await fetch(APIURL);
+  const Info = await response.json();
+  console.log(Info.data);
+  Info.data
+    .filter((skin) => skin.assetPath.includes("/Rifles/"))
+    .forEach((skin) => console.log(skin.displayName));
+}
 
 Rifles();
