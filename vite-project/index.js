0abfd81b73b9d85af067f00422fbab7790c66f81
AndrewL777 <7landrew7@gmail.com>
2023-01-31 20:02:08 -0500

    213123123123123123

--- vite-project/index.js
+++ vite-project/index.js
@@ -1,13 +1,12 @@
 const Dom = {
   men: document.getElementById("men"),
-  veg: document.getElementById("veg"),
-
-
-  // Rifle: document.getElementById("rifle"),
-  // Side: document.getElementById("side"),
-  // Machine: document.getElementById("machine"),
-  // Sub: document.getElementById("sub"),
-  // all: document.getElementById("all"),
+  Rifle: document.getElementById("rifle"),
+  Side: document.getElementById("side"),
+  Machine: document.getElementById("machine"),
+  Sub: document.getElementById("sub"),
+  Sniper: document.getElementById("sniper"),
+  Knives: document.getElementById("knife"),
+  all: document.getElementById("all"),
 };
 
 const APIURL = "https://valorant-api.com/v1/weapons/skins";
@@ -17,16 +16,16 @@ const APIURL = "https://valorant-api.com/v1/weapons/skins";
 const mendiv = document.querySelector("#men");
 
 
-document.getElementById("veg").addEventListener("click", function () {
+document.getElementById("rifle").addEventListener("click", function () {
   mendiv.innerHTML = ``;
 });
 
 
-Dom.veg.addEventListener("click", function () {
-  Vegans();
+Dom.Rifle.addEventListener("click", function () {
+  rifleGuns();
 });
 
-async function Vegans() {
+async function rifleGuns() {
   const response = await fetch(APIURL);
   const Info = await response.json();
   console.log(Info);
@@ -47,56 +46,177 @@ async function Vegans() {
 
 
 
-// document.querySelector("#rifle").addEventListener("click", function () {
-//   displaydiv.innerHTML = "";
-//   rifleGuns();
-// });
+document.getElementById("side").addEventListener("click", function () {
+  mendiv.innerHTML = ``;
+});
 
-// document.getElementById("side").addEventListener("click", function () {
-//   displaydiv.innerHTML = "";
-// });
+Dom.Side.addEventListener("click", function () {
+  sideGuns();
+});
 
-// document.querySelector("#all").addEventListener("click", function () {
-//   displaydiv.innerHTML = "";
-// });
+async function sideGuns() {
+  const response = await fetch(APIURL);
+  const Info = await response.json();
+  console.log(Info);
+  Info.data
+    .filter((skin) => skin.assetPath.includes("/Sidearms/") && !skin.displayName.includes("Standard") && !skin.displayName.includes("Random") ).forEach((skin)=>
+    document.getElementById("men").insertAdjacentHTML(
+      "beforeend",
+      `
+        <div class="card">
+        <h2 class="all">${skin.displayName}</h2>
+  <img class = "images" src="${skin.displayIcon}"  alt = "Image of ${skin.displayName}"  >
+ 
+  </div>
+  `
+    )
+  );
+}
 
 
 
-// document.querySelector("#sub").addEventListener("click", function () {
-//   displaydiv.innerHTML = "";
-// });
 
-// document.querySelector("#sniper").addEventListener("click", function () {
-//   displaydiv.innerHTML = "";
-// });
-// document.querySelector("#machine").addEventListener("click", function () {
-//   displaydiv.innerHTML = "";
-// });
 
-// Dom.Machine.addEventListener("click", function () {
-//   machineGuns();
-// });
 
-// Dom.Sniper.addEventListener("click", function () {
-//   sniperGuns();
-// });
+document.querySelector("#all").addEventListener("click", function () {
+  mendiv.innerHTML = "";
+});
 
-// Dom.all.addEventListener("click", function () {
-//   allGuns();
-// });
+Dom.all.addEventListener("click", function () {
+  allGuns();
+});
 
-// Dom.Sub.addEventListener("click", function () {
-//   subGuns();
-// });
+async function allGuns() {
+  const response = await fetch(APIURL);
+  const Info = await response.json();
+  console.log(Info);
+  Info.data
+    .filter((skin) => skin.assetPath.includes("/Content/") && !skin.displayName.includes("Standard") && !skin.displayName.includes("Random") ).forEach((skin)=>
+    document.getElementById("men").insertAdjacentHTML(
+      "beforeend",
+      `
+        <div class="card">
+        <h2 class="all">${skin.displayName}</h2>
+  <img class = "images" src="${skin.displayIcon}"  alt = "Image of ${skin.displayName}"  >
+ 
+  </div>
+  `
+    )
+  );
+}
 
-// Dom.Rifle.addEventListener("click", function () {
-//   rifleGuns();
-// });
 
-// Dom.Side.addEventListener("click", function () {
-//   sideGuns();
-// });
+document.querySelector("#sub").addEventListener("click", function () {
+  mendiv.innerHTML = "";
+});
 
+Dom.Sub.addEventListener("click", function () {
+  subGuns();
+});
+
+async function subGuns() {
+  const response = await fetch(APIURL);
+  const Info = await response.json();
+  console.log(Info);
+  Info.data
+    .filter((skin) => skin.assetPath.includes("/SubMachineGuns/") && !skin.displayName.includes("Standard") && !skin.displayName.includes("Random") ).forEach((skin)=>
+    document.getElementById("men").insertAdjacentHTML(
+      "beforeend",
+      `
+        <div class="card">
+        <h2 class="all">${skin.displayName}</h2>
+  <img class = "images" src="${skin.displayIcon}"  alt = "Image of ${skin.displayName}"  >
+ 
+  </div>
+  `
+    )
+  );
+}
+
+
+
+document.querySelector("#sniper").addEventListener("click", function () {
+  mendiv.innerHTML = "";
+
+ Dom.Sniper.addEventListener("click", function () {
+    sniperGuns();
+  });
+
+  async function sniperGuns() {
+    const response = await fetch(APIURL);
+    const Info = await response.json();
+    console.log(Info);
+    Info.data
+      .filter((skin) => skin.assetPath.includes("/SniperRifles/") && !skin.displayName.includes("Standard") && !skin.displayName.includes("Random") ).forEach((skin)=>
+      document.getElementById("men").insertAdjacentHTML(
+        "beforeend",
+        `
+          <div class="card">
+          <h2 class="all">${skin.displayName}</h2>
+    <img class = "images" src="${skin.displayIcon}"  alt = "Image of ${skin.displayName}"  >
+   
+    </div>
+    `
+      )
+    );
+  }
+  
+
+
+});
+document.querySelector("#machine").addEventListener("click", function () {
+  mendiv.innerHTML = "";
+});
+
+Dom.Machine.addEventListener("click", function () {
+  machineGuns();
+});
+
+async function machineGuns() {
+  const response = await fetch(APIURL);
+  const Info = await response.json();
+  console.log(Info);
+  Info.data
+    .filter((skin) => skin.assetPath.includes("/HvyMachineGuns/") && !skin.displayName.includes("Standard") && !skin.displayName.includes("Random") ).forEach((skin)=>
+    document.getElementById("men").insertAdjacentHTML(
+      "beforeend",
+      `
+        <div class="card">
+        <h2 class="all">${skin.displayName}</h2>
+  <img class = "images" src="${skin.displayIcon}"  alt = "Image of ${skin.displayName}"  >
+ 
+  </div>
+  `
+    )
+  );
+}
+
+document.querySelector("#knife").addEventListener("click", function () {
+  mendiv.innerHTML = "";
+});
+
+Dom.Knives.addEventListener("click", function () {
+  machineGuns();
+});
+
+async function machineGuns() {
+  const response = await fetch(APIURL);
+  const Info = await response.json();
+  console.log(Info);
+  Info.data
+    .filter((skin) => skin.assetPath.includes("/Melee/") && !skin.displayName.includes("Standard") && !skin.displayName.includes("Random") ).forEach((skin)=>
+    document.getElementById("men").insertAdjacentHTML(
+      "beforeend",
+      `
+        <div class="card">
+        <h2 class="all">${skin.displayName}</h2>
+  <img class = "images" src="${skin.displayIcon}"  alt = "Image of ${skin.displayName}"  >
+ 
+  </div>
+  `
+    )
+  );
+}
 
 
 // Rifles();
